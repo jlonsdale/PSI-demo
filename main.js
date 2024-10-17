e9f1a3e4348ea0046b354a493bafc4b3a0c51daa
jess lonsdale <lonsdale.jess@gmail.com>
2024-10-17 10:03:59 +0100

    batching working - use multiples of m only

--- main.js
+++ main.js
@@ -30,43 +30,13 @@
     // Generate keys and shit //
     ////////////////////////////
 
-    const keyGenerator = seal.KeyGenerator(context);
-    const publicKey = keyGenerator.createPublicKey();
-    const secretKey = keyGenerator.secretKey();
-
-    // Create an Evaluator which will allow HE functions to execute
-    const evaluator = seal.Evaluator(context);
-    // Create a BatchEncoder (only BFV SchemeType)
-    const encoder = seal.BatchEncoder(context);
-    // Create an Encryptor to encrypt PlainTexts
-    const encryptor = seal.Encryptor(context, publicKey);
-    // Create a Decryptor to decrypt CipherTexts
-    const decryptor = seal.Decryptor(context, secretKey);
-
-    return [
-      seal,
-      {
-        publicKey,
-        secretKey,
-        encryptor,
-        decryptor,
-        evaluator,
-        encoder,
-        context,
-      },
-    ];
+    return [seal, context];
   };
+
   const createSets = (n, m) => {
     // Function to generate an array of random numbers
     const generateRandomNumbers = (length) => {
-      return Array.from({ length }, () => Math.floor(Math.random() * 1000) + 1);
-    };
-    const shuffleArray = (array) => {
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1)); // Get a random index from 0 to i
-        [array[i], array[j]] = [array[j], array[i]]; // Swap elements
-      }
-      return array;
+      return Array.from({ length }, () => Math.floor(Math.random() * 1000));
     };
 
     const firstArray = generateRandomNumbers(n);
@@ -76,7 +46,7 @@
     firstArray.push(6100);
     secondArray.push(6100);
 
-    return [shuffleArray(firstArray), shuffleArray(secondArray)];
+    return [firstArray, secondArray];
   };
 
   ///////////////////////////////
@@ -84,20 +54,32 @@
   ///////////////////////////////
 
   let n = 100; // alice set size
-  let m = 10; // bob set size
+  let m = 150; // bob set size
 
   ////////////////////////////
   // Change Batch Size Here //
   ////////////////////////////
 
-  let batch_size = 1;
+  let batch_size = 5;
 
   const [alice_array, bob_array] = createSets(n, m);
-  const [seal_instance, homomorphic_enc_scheme] = await setup();
+
+  const [seal, context] = await setup();
+
+  const keyGenerator = seal.KeyGenerator(context);
+  const publicKey = keyGenerator.createPublicKey();
+  const secretKey = keyGenerator.secretKey();
+  // Create an Evaluator which will allow HE functions to execute
+  const evaluator = seal.Evaluator(context);
+  // Create a BatchEncoder (only BFV SchemeType)
+  const encoder = seal.BatchEncoder(context);
+  // Create an Encryptor to encrypt PlainTexts
+  const encryptor = seal.Encryptor(context, publicKey);
+  // Create a Decryptor to decrypt CipherTexts
+  const decryptor = seal.Decryptor(context, secretKey);
 
   // Step 2
-  const alice_encrypt_elements = ({ alice_array, homomorphic_enc_scheme }) => {
-    const { encryptor, encoder } = homomorphic_enc_scheme;
+  const alice_encrypt_elements = (alice_array) => {
     // Change type of array to work with SEAL
     const set_alice = Int32Array.from(alice_array);
     // Save the length
@@ -118,18 +100,14 @@
     set_ciphertexts_alice_string,
     set_ciphertexts_alice,
     set_alice_length,
-  ] = alice_encrypt_elements({ alice_array, homomorphic_enc_scheme });
+  ] = alice_encrypt_elements(alice_array);
 
   // Step 3
   const bob_homomorphic_operations = (
     set_ciphertexts_alice_string,
     set_alice_length,
-    elements_bob,
-    seal_instance,
-    homomorphic_enc_scheme
+    elements_bob
   ) => {
-    const { evaluator, encoder, context } = homomorphic_enc_scheme;
-    let seal = seal_instance;
     let final_products = [];
     let set_ciphertexts_alice = seal.CipherText();
 
@@ -146,7 +124,6 @@
       sets_plaintexts_bob.push(Int32Array.from(batch));
     }
 
-    let counter = 0;
     sets_plaintexts_bob.forEach((set_plaintexts_bob) => {
       const final_product = seal.CipherText();
 
@@ -185,7 +162,6 @@
       );
 
       const final_product_string = final_product.save();
-      counter++;
       final_products.push(final_product_string);
     });
 
@@ -194,22 +170,11 @@
   let final_products = bob_homomorphic_operations(
     set_ciphertexts_alice_string,
     set_alice_length,
-    bob_array,
-    seal_instance,
-    homomorphic_enc_scheme
+    bob_array
   );
   // Step 4
-  const alice_decrypt_intersection = (
-    final_products,
-    set_alice_length,
-    seal_instance,
-    homomorphic_enc_scheme
-  ) => {
+  const alice_decrypt_intersection = (final_products, set_alice_length) => {
     let intersection_indexes = [];
-    let counter = 1;
-
-    const { decryptor, encoder, context } = homomorphic_enc_scheme;
-    let seal = seal_instance;
 
     for (const final_product of final_products) {
       let final_product_ciphertext = seal.CipherText();
@@ -222,19 +187,14 @@
           intersection_indexes.push(i);
         }
       }
-      counter++;
     }
-
     console.log("Finished PSI calculation in\n");
-
     return intersection_indexes;
   };
 
   let intersection_indexes = alice_decrypt_intersection(
     final_products,
-    set_alice_length,
-    seal_instance,
-    homomorphic_enc_scheme
+    set_alice_length
   );
 
   const process_intersection_indexes = (
